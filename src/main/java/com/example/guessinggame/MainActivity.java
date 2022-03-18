c2da90404c16ddfe0dbe2318e802fd8f79f32178
tranguv <89886131+tranguv@users.noreply.github.com>
2022-03-17 22:52:06 -0400

    Add files via upload

---
+++ src/main/java/com/example/guessinggame/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.guessinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.round1);
+    }
+}
